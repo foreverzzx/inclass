4294e210233afcdecd401e5540961b59a533102b
public <15171615823@163.com>
2016-07-31 23:29:35 +0800

    commit production

---
+++ production/js/common.js
@@ -0,0 +1,10 @@
+function getbaseurl() {
+  //	return "http://localhost:8080/sist/";
+  return "http://www.in-class.cn/";
+}
+
+
+function getlayerurl()
+{
+  return "http://www.in-class.cn/";
+}
---
+++ production/js/jsontime.js
@@ -0,0 +1,25 @@
+﻿//使用说明,da得是字符串,用.toString()转换
+function gettime(da){
+	var date = new Date(parseInt(da.replace("/Date(", "").replace(")/", ""), 10));
+	var year = date.getFullYear();
+	var month = date.getMonth() + 1 < 10 ? "0" + (date.getMonth() + 1) : date.getMonth() + 1;
+	console.log(month);
+	var day = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
+	console.log(day);
+	return year+"-"+month+"-"+day;
+}
+
+function getalltime(da){
+	var date = new Date(parseInt(da.replace("/Date(", "").replace(")/", ""), 10));
+	var year = date.getFullYear();
+	var month = date.getMonth() + 1 < 10 ? "0" + (date.getMonth() + 1) : date.getMonth() + 1;
+	console.log(month);
+	var day = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
+	console.log(day); 
+	var hour = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
+	
+	var min = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+	
+	var sec = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
+	return year+"-"+month+"-"+day+" "+hour+":"+min+":"+ sec; 
+}
